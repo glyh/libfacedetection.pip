a46b36987b40aba79f2da2ef079cd01c7ad25f8a
glyh <lyhokia@gmail.com>
2024-09-27 09:41:20 +0800

    update supported python version

--- setup.py
+++ setup.py
@@ -176,6 +176,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: C++",
         "License :: OSI Approved :: BSD License",
         "Operating System :: POSIX :: Linux",
